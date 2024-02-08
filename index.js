b87f092d89ce8aa6e7d99fb56fa9aaa650a8eef2
alvinyeboah <alvinyeboah5@gmail.com>
2024-02-08 20:35:01 +0000

    Fixed Client side errors, Handled form submission, Ready for data manipulation

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.use(express.static("public"));
+
+app.post("/", function (req, res) {
+  const mealName = req.body.mealname;
+  const qty = req.body.qty;
+  const price = req.body.price;
+  const eatery = req.body.eatery;
+  const totalCost = req.body.totalCost;
+  console.log("yes");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,82 @@
 /* Created on 24 Jan '24 */
+let form = document.getElementById("main");
+document.getElementById("date").innerHTML = d();
+function showModal() {
+  var modal = document.getElementById("myModal");
+  myModal.style.display = "block";
+  setTimeout(() => {
+    myModal.style.display = "none"
+  }, 3000);
+}
 
 function main() {
-    document.getElementById('date').innerHTML = d();
-
-    let form = document.getElementById('main');
-    form.addEventListener('submit', e => {
+  
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    getting_data()
-    })
+    showModal();
+    form.dataset.shouldSubmit = true;
+  });
 }
+main();
 
 function getting_data() {
-    var record = '';
-    var total_cost = 0
+  var record = "";
+  var total_cost = 0;
 
-    let meal_name = field_value('meal_name');
-    let quantity = field_value('quantity');
-    let price = field_value('price');
-    let eatery = field_value('eatery');
-    total_cost += quantity * price;
-    document.getElementById('total_cost').innerHTML = total_cost;
+  let meal_name = field_value("meal_name");
+  let quantity = field_value("quantity");
+  let price = field_value("price");
+  let eatery = field_value("eatery");
+  total_cost += quantity * price;
+  document.getElementById("total_cost").innerHTML = total_cost;
 
-    record += `${d()}, ${meal_name}, ${quantity}, ${price}, ${eatery}, ${String(total_cost)}`;
-    console.log(record);
-    append_to_file(record);
+  record += `${d()}, ${meal_name}, ${quantity}, ${price}, ${eatery}, ${String(
+    total_cost
+  )}`;
+  console.log(record);
 }
-
-function append_to_file(meal_data) {
-    let http = require('http');
-    let fs = require('fs');
-    http.createServer((req, res) => {
-        fs.appendFile('meal_plan_database.txt', meal_data, err => {
-            if (err) throw err;
-            console.log('Updated!');
-        })
-    }).listen(5000);
-}
+// SHould be server side
+// function append_to_file(meal_data) {
+//     let http = require('http');
+//     let fs = require('fs');
+//     http.createServer((req, res) => {
+//         fs.appendFile('meal_plan_database.txt', meal_data, err => {
+//             if (err) throw err;
+//             console.log('Updated!');
+//         })
+//     }).listen(5000);
+// }
 
 function d() {
-    date = new Date().toDateString();
-    return date;
+  date = new Date().toDateString();
+  return date;
 }
 
 function field_value(id) {
-    return document.getElementById(id).value;
+  return document.getElementById(id).value;
 }
 
-main();
+function onClose() {
+  const closeButton = document.querySelector(".close");
+  
+  if (closeButton) {
+    closeButton.addEventListener("click", function () {
+      const modal = document.getElementById("myModal");
+      modal.style.display = "none";
+    //   window.location.reload();
+      if (form.dataset.shouldSubmit === "true") {
+        form.submit();
+        setTimeout(() => {
+        window.location.reload();    
+        }, 2000);
+        
+      }
+    });
+  }
+  
+  
+
+}
+
+onClose();
+
